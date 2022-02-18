c6209355cc9e635711579b68675ed3bae243d21f
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-02-18 15:28:23 -0300

    estudo de forms

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('<int:id>', index, name='index'),
+    path('', home, name='home'),
+    path('create', create, name='create'),
 ]
