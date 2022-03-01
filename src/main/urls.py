b4800a2000e6765d4d69e3143bd6a33e958ad1cd
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2022-03-01 08:42:35 -0300

    finalização do projeto

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('<int:id>', index, name='index'),
     path('', home, name='home'),
+    path('home', home, name='home'),
     path('create', create, name='create'),
+    path('view', view, name='view'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -6,23 +6,27 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get('save'):
-            for item in ls.item_set.all():
-                if response.POST.get('c' + str(item.id)) == 'clicked':
-                    item.complete = True
-                else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get('newItem'):
-            txt = response.POST.get('new')
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print('invalid input')
 
-    return render(response, 'main/list.html', {'ls': ls})
+    if ls in response.user.todolist_set.all():   
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get('save'):
+                for item in ls.item_set.all():
+                    if response.POST.get('c' + str(item.id)) == 'clicked':
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get('newItem'):
+                txt = response.POST.get('new')
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
+                else:
+                    print('invalid input')
+
+        return render(response, 'main/list.html', {'ls': ls})
+    else:
+        return render(response, 'main/view.html', {})
 
 def home(response):
     return render(response, 'main/home.html')
@@ -33,12 +37,14 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data['name']
-            t = ToDoList(name=n)
-            t.save()
+            t = response.user.todolist_set.create(name=n)
         
         return HttpResponseRedirect('/%i' %t.id)
     else:
         form = CreateNewList()
     return render(response, 'main/create.html', {'form': form})
+
+def view(response):
+    return render(response, 'main/view.html', {})
 
 
